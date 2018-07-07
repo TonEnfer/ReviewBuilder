5fd4462fad3baf151f01ed65826138507d962d5f
Mikhail Chernoskutov <mikhail.chernoskutov@gmail.com>
2018-07-07 18:43:42 +0500

    Доделан фронтенд

--- wwwroot/script.js
+++ wwwroot/script.js
@@ -22,13 +22,75 @@ function showAlert(name) {
     return false;
 }
 
+var timerId = undefined;
+
+var tokenId = undefined;
+
 function showModal(token) {
     $("#acquiredToken").text(token);
     $("#loadSuccessModal").modal('show');
+    tokenId = token;
+}
+
+$("#loadSuccessModal").on('shown.bs.modal', function () {
+    startWaitingTimer();
+});
+$("#loadSuccessModal").on('hidden.bs.modal', function () {
+    stopWaitingTimer();
+});
+
+function checkReadyModal() {
+    if(tokenId === undefined){
+        console.log("Something went wrong. Token id is not defined");
+        return;
+    }
+    $.ajax("api/ReviewBuilder/IsReady/" + tokenId, {
+        method: 'GET',
+        dataType: 'json'
+    }
+    ).done(function (data) {
+        console.log(JSON.stringify(data));
+        if (data.isReady) {
+            $("#modalDownloadLink").prop('href', "api/ReviewBuilder/GetFiles/" + tokenId);;
+            $("#modalReadyAlert").collapse('show');
+            stopWaitingTimer();
+            return;
+        }
+        showAlert('processing');
+    }
+    ).fail(function () { showAlert('error'); stopWatchingTimer(); $("#loadSuccessMoadl").modal('hide'); }); 
+}
+
+function startWaitingTimer() {
+    if (timerId === undefined) {
+        timerId = setInterval(checkReadyModal, 1000);
+        console.log("Starting timer " + timerId);
+        return;
+    }
+    console.log("Timer already started");
+}
+function stopWaitingTimer() {
+    if (timerId !== undefined) {
+        clearInterval(timerId);
+        timerId = undefined;
+        console.log("Timer stopped ...");
+    }
+    console.log("No timer to stop...");
 }
 
 function requestStatusFromServer(val) {
-    return 0;
+    $.ajax("api/ReviewBuilder/IsReady/" + val, {
+        method: 'GET',
+        dataType: 'json'
+    }
+    ).done(function (data) {
+        if (data.isReady) {
+            $("#downloadLink").prop('href', "api/ReviewBuilder/GetFiles/" + val); showAlert('ready');
+            return;
+        }
+        showAlert('processing');
+    }
+    ).fail(function () { showAlert('error'); });
 }
 
 $("#fileSubmitForm").on('submit', function (ev) {
@@ -49,12 +111,9 @@ $("#fileSubmitForm").on('submit', function (ev) {
     ).done(function (data) { showAlert('processing'); showModal(data.id); }
     ).fail(function () { showAlert('incorrectFile'); });
 
-    // $("#modalReadyAlert").collapse('show');
 
 });
-
 $("#tokenSubmitForm").on('submit', function (ev) {
     status = requestStatusFromServer($("#inputToken").val());
-    showAlert(status);
     ev.preventDefault();
 });
